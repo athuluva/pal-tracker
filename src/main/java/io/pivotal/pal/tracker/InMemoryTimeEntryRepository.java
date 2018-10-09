5a985805c075e95e5de4b9acedeed1f3a3d55010
hongxiao20 <jxiao1@ford.com>
2018-10-09 16:55:56 -0400

    Lab 4 fixed test List()

--- src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
@@ -1,12 +1,9 @@
 package io.pivotal.pal.tracker;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import static java.util.Arrays.asList;
 
 public class InMemoryTimeEntryRepository implements TimeEntryRepository {
 
@@ -27,19 +24,14 @@ public class InMemoryTimeEntryRepository implements TimeEntryRepository {
     }
 
     public List<TimeEntry> list() {
-        List<TimeEntry> list = asList( (TimeEntry)map.get(1L), (TimeEntry)map.get(2L));
+        List<TimeEntry> list = new ArrayList<>();//asList( (TimeEntry)map.get(1L), (TimeEntry)map.get(2L));
 
         // using for-each loop for iteration over Map.entrySet()
+        int i=0;
         for (Map.Entry<Object, Object> entry : map.entrySet()) {
 
-            list.add((TimeEntry)map.get(entry.getKey()));
+            list.add(i++, (TimeEntry)map.get(entry.getKey()));
         }
-            /*System.out.println("Key = " + entry.getKey() +
-                    ", Value = " + entry.getValue());*/
-
-        /*new ArrayList<>() ;
-        list.add(0,(TimeEntry)map.get(1L));
-        list.add(1,(TimeEntry)map.get(2L));*/
 
         return list;
     }
